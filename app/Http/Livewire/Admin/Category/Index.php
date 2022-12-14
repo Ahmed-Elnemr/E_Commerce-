65ff9ae2d92dadc28f4aac853f4c0c1446347276
Ahmed-Elnemr <ahmedelnemr064@gmail.com>
2022-12-14 13:59:24 +0200

    add delete Category and deleted modal

--- app/Http/Livewire/Admin/Category/Index.php
+++ app/Http/Livewire/Admin/Category/Index.php
@@ -6,19 +6,41 @@ use Livewire\Livewire;
 use Livewire\Component;
 use App\Models\Category;
 use Livewire\WithPagination;
+use Illuminate\Support\Facades\File;
 
 class Index extends Component
 {
     use WithPagination;
-
     protected $paginationTheme = 'bootstrap';
 
 
+    public $category_id;
+    //get CategoryId
+    public function deleteCategory($category_id)
+    {
+        // dd($category_id);
+        $this->category_id = $category_id;
+    }
+    //dlete Category by $category_id
+    public function destroyCategory(){
+        $category=Category::find($this->category_id);
+        $path='uploads/categorys/'.$category->image;
+        if(File::exists($path)){
+            File::delete($path);
+        }
+        $category->delete();
+        session()->flash('message', 'Category deleted successfully');
+        $this->dispatchBrowserEvent('close-modal');
+    }
+
+
+
+    ///render category
     public function render()
     {
         $categories = Category::orderBy('id', 'asc');
         return view('livewire.admin.category.index', [
-            'categories' => $categories->paginate(4) ,
+            'categories' => $categories->paginate(4),
         ]);
     }
 }
--- resources/views/livewire/admin/category/index.blade.php
+++ resources/views/livewire/admin/category/index.blade.php
@@ -1,14 +1,39 @@
 <div>
-    @if (session('message'))
-        <h3 class="alert alert-success">{{ session('message') }}</h3>
-    @endif
+
+    <div wire:ignore.self class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Delete Category ?!</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                {{-- //form for delete --}}
+                <form wire:submit.prevent="destroyCategory">
+                    <div class="modal-body">
+                        <h5>Are tou sure delete This Category ?</h5>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-danger">Yes Delete </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
 
     <div class="row">
+        @if (session('message'))
+            <h3 class="alert alert-success">{{ session('message') }}</h3>
+        @endif
+
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
                     <h4>Category
-                        <a href="{{ route('category.create') }}" class="btn btn-primary btn-sm float-end">Add Category</a>
+                        <a href="{{ route('category.create') }}" class="btn btn-primary btn-sm float-end">Add
+                            Category</a>
                     </h4>
                 </div>
                 <div class="card-body">
@@ -28,8 +53,11 @@
                                     <td>{{ $category->name }}</td>
                                     <td>{{ $category->status == '1' ? 'Hidden' : 'Visable' }}</td>
                                     <td>
-                                        <a href="{{route('category.edit',$category->id)}}" class="btn btn-success">Edite</a>
-                                        <a href="" class="btn btn-danger">Delete</a>
+                                        <a
+                                            href="{{ route('category.edit', $category->id) }}"class="btn btn-success">Edite</a>
+                                        <a href="#" wire:click="deleteCategory({{ $category->id }})"
+                                            data-bs-toggle="modal"
+                                            data-bs-target="#deleteModal"class="btn btn-danger">Delete</a>
                                     </td>
                                 </tr>
                             @endforeach
@@ -44,3 +72,11 @@
         </div>
     </div>
 </div>
+
+@push('script')
+<script>
+    window.addEventListener('close-modal', event=>{
+        $('#deleteModal').modal('hide');
+    })
+</script>
+@endpush
